e15d9783a63073a5c18fc15a279fd00a0ea1b19f
lucasliet <lucasouliveira@gmail.com>
2020-06-29 20:08:01 -0300

    create repository

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,21 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    return transactions.reduce(
+      (acc: Balance, current) => {
+        if (current.type === 'income') {
+          acc.income += current.value;
+          acc.total += current.value;
+        } else {
+          acc.outcome += current.value;
+          acc.total -= current.value;
+        }
+        return acc;
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
   }
 }
 
